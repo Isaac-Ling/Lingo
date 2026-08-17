32eecab8fc2b9f43eed6e2630a565353e80b0aaa
Isaac Ling <iling3434@gmail.com>
2026-08-17 13:31:22 +0100

    Path induction neutrality bug fix and path identities

--- src/Core/Judgement/Evaluation.hs
+++ src/Core/Judgement/Evaluation.hs
@@ -76,7 +76,7 @@ isNeutral (Ind (Sum _ _) _ [Bind x (NoBind c), Bind y (NoBind d)] (Inl a))      
 isNeutral (Ind (Sum _ _) _ [Bind x (NoBind c), Bind y (NoBind d)] (Inr b))       = False
 isNeutral (Ind Nat _ [NoBind c0, _] Zero)                                        = False
 isNeutral (Ind Nat m [c0, Bind x (Bind y (NoBind cs))] (Succ n))                 = False
-isNeutral (Ind (IdFam t) m [Bind z (NoBind c), NoBind a, NoBind a'] (Refl ma'')) = a == a' && maybe True (a' ==) ma''
+isNeutral (Ind (IdFam t) m [Bind z (NoBind c), NoBind a, NoBind a'] (Refl ma'')) = not (a == a' && maybe True (a' ==) ma'')
 isNeutral (Ind t m c a)                                                          = isValue t && isBoundTermValue m && all isBoundTermValue c && isValue a
   where
     isBoundTermValue :: BoundTerm -> Bool
