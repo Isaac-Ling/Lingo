1df8a717fcb304c82bb491821dcc984768350559
Isaac Ling <iling3434@gmail.com>
2026-09-04 21:31:13 +0100

    Fix iterated pi binder order

--- src/IO/Pretty.hs
+++ src/IO/Pretty.hs
@@ -79,12 +79,12 @@ showTermWithBinders False bs (Pi (Just x, t, Imp) m)            = showTermWithBi
 showTermWithBinders False bs (Pi (Nothing, t, Imp) m)           = showTermWithBinders False (Nothing : bs) m
 showTermWithBinders b bs (Pi (Nothing, Pi (y, t, ex') m, ex) n) = showExLParen ex ++ showTermWithBinders b bs (Pi (y, t, ex') m) ++ showExRParen ex ++ " -> " ++ showTermWithBinders b (Nothing : bs) n
 showTermWithBinders b bs (Pi (Just x, t, ex)
-  (Pi (Just y, t', ex') m)) | t == bumpDown t' && ex == ex'     = showIteratedPis (Just x : Just y : bs) ex t [y, x] m
+  (Pi (Just y, t', ex') m)) | t == bumpDown t' && ex == ex'     = showIteratedPis (Just y : Just x : bs) ex t [y, x] m
   where
     showIteratedPis :: Binders -> Explicitness -> Term -> [ByteString] -> Term -> String
     showIteratedPis bs' ex t vars (Pi (Just x, t', ex') m)
       | t == shift (-(length vars)) t' && ex == ex' = showIteratedPis (Just x : bs') ex t (x : vars) m
-    showIteratedPis bs' ex t vars m   = showExLParen ex ++ intercalate ", " (reverse $ map unpack vars) ++ " : " ++ showTermWithBinders b bs t ++ showExRParen ex ++ " -> " ++ showTermWithBinders b bs' m
+    showIteratedPis bs' ex t vars m                 = showExLParen ex ++ intercalate ", " (reverse $ map unpack vars) ++ " : " ++ showTermWithBinders b bs t ++ showExRParen ex ++ " -> " ++ showTermWithBinders b bs' m
 showTermWithBinders b bs (Pi (Just x, t, ex) m)                 = showExLParen ex ++ unpack x ++ " : " ++ showTermWithBinders b bs t ++ showExRParen ex ++ " -> " ++ showTermWithBinders b (Just x : bs) m
 showTermWithBinders b bs (Pi (Nothing, t, ex) m)                = showTermWithBinders b bs t ++ " -> " ++ showTermWithBinders b (Nothing : bs) m
 showTermWithBinders b bs (Succ m)
